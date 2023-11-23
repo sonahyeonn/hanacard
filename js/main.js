9c9383f9c0ad759152026bb237c37868a0aee820
ahyeon <koinohibi@gmail.com>
2023-11-23 09:59:49 +0900

    세번번째 수정 2023/11/23

--- js/main.js
+++ js/main.js
@@ -1,10 +1,77 @@
-// slick
+    // 📌slick
+    let eb_slider = $('.eb_slider_list')
+    eb_slider.slick({
+      arrows: false,
+      dots: true,
+      autoplay: true,
+      pauseOnHover: false,
+      fade: true,
+    });
 
-$('.eb_slider_list').slick({
-  arrows:false,
-  dots:true,
-  autoplay:true,
-  pauseOnHover:false,
-  });
+    // 📌정지/재생 버튼
+    // 선택자.addEventListerner('click',function(){})
+    // 선택자.onclick(function(){})
+    $('.event_box2 .page_act .btn_stop').click(function (e) {
+      e.preventDefault();
+      if ($('.event_box2 .page_act').hasClass('on')) {
+        //on이라는 클래스명을 가지고 있다면 true, 없다면 false
+        $('.event_box2 .page_act').removeClass('on');
+        eb_slider.slick('slickPause');
 
+      }
+    })
+
+    $('.event_box2 .page_act .btn_play').click(function (e) {
+      e.preventDefault();
+      if (!$('.event_box2 .page_act').hasClass('on')) {
+
+        //! --> not   !! --> 긍정   
+        $('.event_box2 .page_act').addClass('on');
+        eb_slider.slick('slickPlay');
+      }
+    })
+
+    // 📌card_slick
+    $('.card').slick({
+      arrows: false,
+      dots: true,
+      autoplay: true,
+      pauseOnHover: false,
+      fade: true,
+    });
+
+    // 📌정지/재생 버튼
+    // 선택자.addEventListerner('click',function(){})
+    // 선택자.onclick(function(){})
+    $('.event_box3 .page_act .btn_stop').click(function (e) {
+      e.preventDefault();
+      if ($('.event_box3 .page_act').hasClass('on')) {
+        //on이라는 클래스명을 가지고 있다면 true, 없다면 false
+        $('.event_box3 .page_act').removeClass('on');
+        $('.card').slick('slickPause');
+
+      }
+    })
+
+    $('.event_box3 .page_act .btn_play').click(function (e) {
+      e.preventDefault();
+      if (!$('.event_box3 .page_act').hasClass('on')) {
+
+        //! --> not   !! --> 긍정   
+        $('.event_box3 .page_act').addClass('on');
+        $('.card').slick('slickPlay');
+      }
+    })
+
+    // 📌life 버튼
+    let tab=$('.life_menu ul li');
+    let menu=$('.life_style ul li');
+
+    tab.mouseover(function(){
+      let target = $(this);
+      let index = target.index();
+      console.log(index)
+      menu.css({opacity:0})
+      menu.eq(index).css({opacity:1})
+    });
 
